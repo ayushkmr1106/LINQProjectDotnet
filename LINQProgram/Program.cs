f95f64d399fb6fd9a4e5aa7087b73181bd7d4270
Ayush Kumar <ayushkmr1106@gmail.com>
2026-02-05 21:23:51 +0530

    All, Any, Contains Method | Quantifier Operations | WebGentle

--- LINQProgram/Program.cs
+++ LINQProgram/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 namespace LINQProgram
 {
@@ -6,83 +8,104 @@ namespace LINQProgram
     {
         static void Main(string[] args)
         {
-            // Sorting Operators 
-            // There are five methods in Sorting:
-            // 1. OrderBy: Sort data in ascending order.
-            // 2. OrderByDescending: Sort data in descending order.
-            // 3. ThenBy
-            // 4. ThenByDescending
-            // 5. Reverse
-
-            // OrderBy
-            var dataSourceInt = new List<int>() { 5, 12, 13, 1, 8, 33, 20 };
-
-            var dataSourceString = new List<string>()
+            // Quantifier Operations
+            // Quantifier Operations are used on a data source to check if some or all elements of that data source satisfy a condition or not.
+            // All the methods in quantifier ops return a boolean value.
+            // Condition may be for some or all the elements.
+            // All, Any, Contains
+            Student[] students =
             {
-                "Smith",
-                "Anderson",
-                "Wright",
-                "Mitchell",
-                "Thomas",
-                "Allen",
-                "Evans",
-                "Collins"
+                new Student() {Name= "John", Marks=90,
+                Subject = new List<Subject>() {
+                    new Subject() { SubjectName = "Math", SubjectMarks=89},
+                    new Subject() { SubjectName = "Hindi", SubjectMarks=79},
+                    new Subject() { SubjectName = "English", SubjectMarks=54},
+                } },
+                new Student() {Name= "Ava", Marks=80,
+                Subject = new List<Subject>() {
+                    new Subject() { SubjectName = "Math", SubjectMarks=86},
+                    new Subject() { SubjectName = "Hindi", SubjectMarks=93},
+                    new Subject() { SubjectName = "English", SubjectMarks=78},
+                }   },
+                new Student() {Name= "Sid", Marks=75,
+                Subject = new List<Subject>() {
+                    new Subject() { SubjectName = "Math", SubjectMarks=75},
+                    new Subject() { SubjectName = "Hindi", SubjectMarks=84},
+                    new Subject() { SubjectName = "English", SubjectMarks=95},
+                }   },
             };
 
-            var dataSource = new List<Employee>()
+            List<Employee> employee = new()
             {
-                new Employee()
-                {
-                    Id = 3,
-                    Email = "Smith@abc.co",
-                    LastName = "Smith",
-                    FirstName = "Foo"
-                },
-                new Employee()
-                {
-                    Id = 2,
-                    Email = "Thomas@abc.co",
-                    LastName = "Thomas",
-                    FirstName = "Mark"
-                },
-                new Employee()
-                {
-                    Id = 1,
-                    Email = "Allen@email.com",
-                    LastName = "Allen",
-                    FirstName = "Mark"
-                },
-                new Employee()
-                {
-                    Id = 4,
-                    Email = "Anderson@email.com",
-                    LastName = "Anderson",
-                    FirstName = "Foo"
-                }
+                new Employee() { Id = 1, FirstName = "Kim"},
+                new Employee() {Id = 2, FirstName = "John" , LastName ="Stark", Email = "def@xyz.co"},
+
             };
 
-            // Reverse Operator
-            int[] rollNums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var comparer = new EmployeeComparer();
 
-            var methodSyntax = rollNums.Reverse();
+            //All Operator
+            //bool methodSyntax = students.All(student => student.Marks > 70);
 
-            // Mixed Syntax
-            var querySyntax = (from number in rollNums
-                               select number).Reverse();
-            
-            // System.Collections.Generic namespace - In-Place Sorting
-            // dataSourceString.Reverse();
-            
-            // System.Linq namespace - Out-Of-Place Sorting
-            var qs = dataSourceString.AsEnumerable().Reverse();
-            
-            foreach (var item in qs)
-            {
-                Console.WriteLine(item);
-            }
+            //var querySyntax = (from student in students
+            //                   select student).All(student => student.Marks > 70);
+
+            var methodAllSyntax = students.Where(student => student.Subject.All(subject => subject.SubjectMarks > 76)).Select(student => student.Name).ToList();
+
+            var queryAllSyntax = (from std in students
+                              where std.Subject.All(x => x.SubjectMarks > 76)
+                              select std.Name).ToList();
+
+            // Any Operator
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5 };
+            // Check if any elememnt is present in list
+            bool isAvailable = numbers.Any();
+
+            //var methodAnySyntax = students.Where(std => std.Marks >= 80).Select(student => student.Name).ToList();
+
+            var methodAnySyntax = students.Where(std => std.Subject.Any(sub => sub.SubjectMarks > 94)).Select(std => std.Name).ToList();
+
+            // Contains Operator
+            List<string> employees = new() { "Kim", "Sam", "Vicky", "Michell" };
+            var isExist = employees.AsEnumerable().Contains("Sam");
+
+            var isExist2 = employee.Contains(new Employee() { Id = 1, FirstName = "Kim"}, comparer);
 
 
             Console.ReadLine();
         }
+
+        class EmployeeComparer : IEqualityComparer<Employee>
+        {
+            public bool Equals(Employee? x, Employee? y)
+            {
+                if (object.ReferenceEquals(x , y))
+                {
+                    return true;
+                }
+                if (object.ReferenceEquals(x , null) || object.ReferenceEquals(y , null))
+                {
+                    return false;
+                }
+
+                return x.Id == y.Id && x.FirstName == y.FirstName && x.LastName == y.LastName && x.Email == y.Email;
+            }
+
+            public int GetHashCode([DisallowNull] Employee obj)
+            {
+                if (object.ReferenceEquals(obj, null))
+                {
+                    return 0;
+                }
+
+                int idHashCode = obj.Id.GetHashCode();
+                int firstNameHashCode = obj.FirstName == null ? 0: obj.FirstName.GetHashCode();
+                int lastNameHashCode = obj.LastName == null ? 0 : obj.LastName.GetHashCode();
+                int emailHashCode = obj.Email == null ? 0 : obj.Email.GetHashCode();
+
+                return idHashCode ^ firstNameHashCode ^ lastNameHashCode ^ emailHashCode;
+            }
+        }
+
     }
 }
---
+++ LINQProgram/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProgram
+{
+    internal class Student
+    {
+        public string? Name { get; set; }
+        public int Marks { get; set; }
+        public List<Subject>? Subject {  get; set; }
+    }
+
+    internal class Subject()
+    {
+        public string? SubjectName { get; set; }
+        public int SubjectMarks { get; set; }
+    }
+}
